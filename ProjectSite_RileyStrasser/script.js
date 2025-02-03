1eee7851eed4d52b30e4c5db61b64255e72ff8c4
RileyStrasser <144822253+rstra9564@users.noreply.github.com>
2025-02-02 17:35:38 -0700

    Add files via upload

---
+++ ProjectSite_RileyStrasser/script.js
@@ -0,0 +1,7 @@
+/*Riley Strasser 2/2/25*/
+alert("Welcome To STRAÜSS Music"); 
+
+function myFunction() {
+    document.getElementById("ticketbutton").innerHTML = "No Tickets Available at this time.";
+}
+
