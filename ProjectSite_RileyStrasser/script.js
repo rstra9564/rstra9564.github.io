75609a5551555a2a3fbe278ffb301b581c158c54
RileyStrasser <144822253+rstra9564@users.noreply.github.com>
2025-02-16 19:08:10 -0700

    Delete ProjectSite_RileyStrasser directory

--- ProjectSite_RileyStrasser/script.js
+++
@@ -1,7 +0,0 @@
-/*Riley Strasser 2/2/25*/
-alert("Welcome To STRAÜSS Music"); 
-
-function myFunction() {
-    document.getElementById("ticketbutton").innerHTML = "No Tickets Available at this time.";
-}
-
