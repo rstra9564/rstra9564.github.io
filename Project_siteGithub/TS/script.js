5ffd3877eda31cabc8a8edda29660eb722f0f0fc
RileyStrasser <144822253+rstra9564@users.noreply.github.com>
2025-03-03 00:01:35 -0700

    Add files via upload

---
+++ Project_siteGithub/TS/script.js
@@ -0,0 +1,44 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function myFunction() {
+    document.getElementById("ticket-button").innerHTML = "No Tickets Available at this time.";
+}
+function validateForm() {
+    nameError.textContent = "";
+    emailError.textContent = "";
+    messageError.textContent = "";
+    if (name.trim() === "") {
+        nameError.textContent = "Name is required";
+        isValid = false;
+    }
+    if (email.trim() === "") {
+        emailError.textContent = "Email is required";
+        isValid = false;
+    }
+    else if (!isValidEmail(email)) {
+        emailError.textContent = "Invalid email format";
+        isValid = false;
+    }
+    if (message.trim() === "") {
+        messageError.textContent = "Message is required";
+        isValid = false;
+    }
+    return isValid;
+}
+function isValidEmail(email) {
+    return emailRegex.test(email);
+}
+/**/
+scrollContainer.addEventListener("wheel", function (evt) {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+    scrollContainer.style.scrollBehavior = "auto";
+});
+nextBtn.addEventListener("click", function () {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += 900;
+});
+backBtn.addEventListener("click", function () {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft -= 900;
+});
